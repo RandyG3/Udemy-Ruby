253a089295efc1db5a46fcaceb57fed9881ba2bd
“RandyG3” <tbear03@comcast.net>
2019-06-10 10:44:37 -0400

    This is a commit

---
+++ example.rb
@@ -0,0 +1,15 @@
+puts "hello world!"
+puts "another line"
+
+puts "I bought 5 dollars at the store."
+puts "I made $65 dollars or a profit of 25%!"
+
+puts 5
+puts 3.14159
+
+puts 4 + 3
+puts "4" + "3"
+
+puts
+puts
+puts "See, there's a bunch of line breaks above."
