5af4ef9ac91853e46a7de43d5b2998f957b25041
RandyG3 <40631249+RandyG3@users.noreply.github.com>
2019-12-19 11:58:54 -0500

    New file structure and moving files to correct folders

---
+++ Section 03 Numbers and Booleans/Arithmetic_Methods_and_Basic_Args.rb
@@ -0,0 +1,33 @@
+p 1 + 4
+
+p 10 - 6
+p 6 - 10
+
+p 6 * 6
+
+p "School" + "bus"
+p "School " + "bus"
+
+p 10 / 5 
+p 12 / 5
+
+p 12.0 / 5
+p 12 / 5.0
+p 12.0 / 5.0
+
+p 2 * 2 * 2 * 2
+p 2 ** 4
+
+p 5 % 2 # (2 / 5) remainder is 1
+p 6 % 2 # no remainder, answer is 0
+
+# caveat about division if dividing small, put 0 in front ie 0.5 /2
+p 12 / 5
+p 10 / 5
+# p .5  / 2 gives an error
+p 0.5 / 2
+
+p 5 % 2
+p 6 % 2
+
+p 23 % 4
---
+++ Section 03 Numbers and Booleans/Less_Than_and_Greater_Than.rb
@@ -0,0 +1,12 @@
+# T/F return
+p 1 < 8 
+p 10 < 15
+
+p 12 < 5
+
+p 1 <= 5
+p 13 <= 5
+
+p 5 < 5 # False
+
+# > & >= works the same way - go figure!!
